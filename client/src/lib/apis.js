9fd05314b3214bc175a5d0f467ed75a6f645efda
Kamal Kumar <kamalkumar@Kamals-MacBook-Air.local>
2026-04-04 15:57:34 +0530

    updates to deploy on netlilfy

--- client/src/lib/apis.js
+++ client/src/lib/apis.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = import.meta.env.VITE_BACKEND_BASE_URL;
 
 function getAuthHeaders() {
   const raw = localStorage.getItem("token");
